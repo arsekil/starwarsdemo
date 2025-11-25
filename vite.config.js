eaa01bf68c13c2696dcc388d1cea67805058ed3e
Ciocan Paul Andrei <ciocanpa87@gmail.com>
2025-11-25 19:18:47 +0100

    add Netlify preview server into Vite Config server.allowedHosts

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ['devserver-main--starwarsmoviesdemo.netlify.app'],
+  },
   test: {
     globals: true,
     environment: 'jsdom',
